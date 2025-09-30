8f6e47a1ec99f7bb770e55269de951046e2b5fa0
nahoryi-danylo <daniilnagornyi3202@gmail.com>
2025-09-30 18:21:05 +0300

    lab1 probably final

--- lab1/build.bat
+++ lab1/build.bat
@@ -1,2 +1,1 @@
-windres resource.rc -O coff -o resource.res
-g++ -o lab1.exe lab1.cpp resource.res -mwindows -luser32 -lgdi32 -municode
+g++ -o lab1.exe lab1.cpp -mwindows -luser32 -lgdi32 -municode
