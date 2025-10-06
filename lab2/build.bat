c237021d9ac10097c16c249fb0be9cfbc6cb1ccf
nahoryi-danylo <daniilnagornyi3202@gmail.com>
2025-10-06 13:52:58 +0300

    blah blah

--- lab2/build.bat
+++ lab2/build.bat
@@ -1,2 +1,2 @@
 windres resource.rc -O coff -o resource.res
-g++ -o lab2.exe lab2.cpp resource.res -mwindows -luser32 -lgdi32 -municode
+g++ -o lab2.exe lab2.cpp graph.cpp -mwindows -luser32 -lgdi32 -municode
